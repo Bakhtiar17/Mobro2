80065c95f04af17e9e57cc60397926c8964defd7
MuhammadEkaBakhtiar <bakhtiar@student.telkomuniversity.ac.id>
2022-11-16 18:44:03 +0700

    mod8

--- app/src/main/java/org/mobro4046/mobro2/MainViewModelFactory.kt
+++ app/src/main/java/org/mobro4046/mobro2/MainViewModelFactory.kt
@@ -6,7 +6,7 @@ import org.mobro4046.mobro2.data.MahasiswaDao
 
 class MainViewModelFactory(private val dataSource: MahasiswaDao) :
     ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel > create(modelClass: Class<T>): T {
         if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
             return MainViewModel(dataSource) as T
